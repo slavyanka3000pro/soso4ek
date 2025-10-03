5e5799701adc7be20cc86332a7507955d388b3ac
uczen <uczen@IGOR>
2025-10-03 15:36:44 +0200

    class weather

---
+++ StacjaPogodowa/DataBase/weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    internal class Weather
+    {
+        public int Id { get; set; }
+
+        public string MeasueredBy { get; set; } = string.Empty;
+
+        public DateTime MeasueredDate { get; set;}
+    }
+}

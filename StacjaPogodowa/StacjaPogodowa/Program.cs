a7d05a817fa6c9b7db60862df4ac02f555f12341
slavyanka3000pro <slavyaningl@gmail.com>
2025-10-24 15:39:30 +0200

    Update Program.cs
    updated main program

--- StacjaPogodowa/StacjaPogodowa/Program.cs
+++ StacjaPogodowa/StacjaPogodowa/Program.cs
@@ -1,1 +1,21 @@
+// See https://aka.ms/new-console-template for more information
+
+
+using Database;
+
+Console.WriteLine("Hello, World!"); 
+Console.ReadKey();
+Weather weather1 = new Weather();
+Weather weather5= new Weather();
+weather5= ChangeWeather(weather1);
+
+
+Weather ChangeWeather(Weather weather)
+{
+    var objectToChange = new Weather();
+    Console.WriteLine($"Weather measured by: {weather.MeasuredBy}");
+    weather.MeasuredBy = "Jan Kowalski";
+    Console.WriteLine($"Weather measured by:{weather.MeasuredBy}");
+    return objectToChange;
+}
 ﻿
